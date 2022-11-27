d048ac2919a5fa8e55dca17665ca288b844a6fd3
mrange <marten_range@hotmail.com>
2022-11-27 12:55:42 +0100

    License

--- source/FsNICC.WPF/WPFRenderer.fs
+++ source/FsNICC.WPF/WPFRenderer.fs
@@ -1,4 +1,19 @@
-﻿module FsNICC.WPF.WPFRenderer
+﻿(*
+Copyright 2022 Mårten Rånge
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*)
+module FsNICC.WPF.WPFRenderer
 open System
 open System.Collections.Generic
 open System.Globalization
@@ -132,7 +147,7 @@ let toWPFScene (scene : Scene) : WPFScene =
     let brush = knownBrushes.[rgb]
     let pf    = PathFigure ()
 
-    let rec loop (pf : PathFigure) (vvs : Vertex2D array array) i = 
+    let rec loop (pf : PathFigure) (vvs : Vertex2D array array) i =
       if i < vvs.Length then
         let vs    = vvs.[i]
         let pts   = Array.zeroCreate (vs.Length)
@@ -196,8 +211,8 @@ let toWPFScene (scene : Scene) : WPFScene =
     { Polygons = polygons }
 
 
-  let wpfFrames = 
-    frames 
+  let wpfFrames =
+    frames
     |> Array.map mapFrame
 
   { Frames = wpfFrames }
